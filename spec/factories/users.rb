3273b4b87b470e3ccf2838d9048936e38cd8620e
huykhuong <scottkhuong@gmail.com>
2024-04-21 21:13:21 +0700

    Factories and sessions spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,14 @@ FactoryBot.define do
     username { "StrawberryCookie" }
     email { "test@email.com" }
     password { "testpassword" }
+
+    factory :confirmed_user do
+      confirmed_at { "Wed, 17 Apr 2024 15:24:31.395409000 UTC +00:00" }
+    end
+
+    factory :duplicated_user do
+      username { 'strawberrycookie' }
+      email { 'huy@email.com' }
+    end
   end
 end
