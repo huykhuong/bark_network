d5073812c1350c7e0fee851e20e1fbb0eb59a866
huykhuong <46696683+huykhuong@users.noreply.github.com>
2024-04-27 19:57:31 +0700

    Add specs (models & requests) for profiles (#14)
    * Update yaml
    
    * Factories and models specs and request specs
    
    * Update SessionHelper

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -1,7 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe "Profiles", type: :request do
-  describe "GET /index" do
+  let (:user) { create(:user) }
 
+  before do |test|
+    login user unless test.metadata[:skip_before]
+  end
+
+  describe "GET /profile" do
+    specify do
+      get '/profile'
+      expect(response).to be_successful
+      expect(controller.action_name).to eq('edit')
+      expect(response).to render_template('edit')
+    end
+  end
+
+  describe "POST /profile" do
+    context 'Successful profile post request' do
+      let (:params) { {  profile: attributes_for(:profile) } }
+
+      specify 'With setup profile' do
+        post '/profile', params:;
+        expect(response).to be_successful
+        expect(controller.action_name).to eq('update')
+        expect(response.parsed_body[:data][:message]).to eq('Your profile has been updated')
+      end
+
+      specify 'With profile not yet setup', :skip_before do
+        login(user, profile_set_up: false)
+        post '/profile', params:;
+        expect(response).to be_successful
+        expect(controller.action_name).to eq('update')
+        expect(response.parsed_body[:data][:message]).to include('you are now ready to join the Bark community')
+      end
+    end
+
+    context 'Failed profile post request' do
+      let (:params) { {  profile: { bio: 'a' * 251 , display_name: 'huy space', gender: 'random', date_of_birth: Date.tomorrow} } }
+
+      specify 'Invalid params' do
+        post '/profile', params:;
+        expect(response).to_not be_successful
+        expect(response.parsed_body[:errors][:gender]).to eq('random is not a valid gender.')
+        expect(response.parsed_body[:errors][:dateOfBirth]).to eq("Date of birth can't be in the future")
+        expect(response.parsed_body[:errors][:displayName]).to eq("Display name can't contain white spaces.")
+        expect(response.parsed_body[:errors][:bio]).to eq("Bio is too long (maximum is 250 characters).")
+      end
+
+      specify 'Missing DOB' do
+        params.tap { |p| p[:profile][:date_of_birth] = nil }
+        post '/profile', params:;
+        expect(response).to_not be_successful
+        expect(response.parsed_body[:errors][:dateOfBirth]).to eq("Please provide your date of birth.")
+      end
+    end
   end
 end
