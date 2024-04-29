eaf3a56001d0383587472af184987d4dbe1a5a31
huykhuong <scottkhuong@gmail.com>
2024-04-29 22:28:20 +0700

    Add specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context 'Passed validations' do
+      let(:post) { build(:post) }
+
+      specify 'a valid post' do
+        expect(post).to be_valid
+      end
+    end
+
+    context 'Failed validations' do
+      let(:post) { build(:post, author: nil, content: nil, title: "a" * 256) }
+
+      specify 'not a valid post without content' do
+        expect(post).to_not be_valid
+        expect(post.errors[:content]).to include("You forgot to add some content for the post.")
+      end
+
+      specify 'not a valid post with too long title' do
+        expect(post).to_not be_valid
+        expect(post.errors[:content]).to include("You forgot to add some content for the post.")
+      end
+
+      specify 'no author assigned' do
+        expect(post).to_not be_valid
+        expect(post.errors[:author]).to include("A post must have an author.")
+      end
+    end
+
+    context 'Post has been edited' do
+      let(:post) { create(:post) }
+
+      specify 'post has been edited' do
+        post.update(title: "New Title")
+        expect(post.edited?).to be true
+      end
+    end
+
+    context 'Post belongs to an author' do
+      let(:user) { build(:user) }
+      let(:post) { build(:post) }
+
+      specify do
+        expect(post.author).to eq(user)
+      end
+    end
+  end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,18 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/posts/index"
-      expect(response).to have_http_status(:success)
-    end
+  let(:user) { create(:user) }
+
+  before do
+    login(user)
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/posts/show"
-      expect(response).to have_http_status(:success)
+  context "createPost mutation" do
+    let(:post_data) { response.parsed_body['data']['createPost']['post'] }
+
+    specify "Should create a new post" do
+      graphql_query = <<~GRAPHQL
+        mutation {
+          createPost(title: "Test Title", content: "Test Content") {
+            post {
+              authorName
+              title
+              content
+              edited
+              createdAt
+            }
+          }
+        }
+      GRAPHQL
+
+      post '/graphql', params: { query: graphql_query }
+      expect(response).to be_successful
+      expect(post_data['authorName']).to eq(user.profile.display_name)
+      expect(post_data['title']).to eq("Test Title")
+      expect(post_data['content']).to eq("Test Content")
+      expect(post_data['edited']).to be false
+      expect(post_data['createdAt']).to be_present
+      expect(Post.count).to eq(1)
     end
   end
-
 end
