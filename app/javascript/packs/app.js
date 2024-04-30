4681f49bcd4e73d8509f4e6a7917aeef749410ac
huykhuong <scottkhuong@gmail.com>
2024-04-30 22:41:56 +0700

    Init time ago

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -2,6 +2,12 @@ import ReactRailsUJS from "react_ujs";
 
 import * as Components from "../app/**/*.tsx";
 
+import TimeAgo from "javascript-time-ago";
+
+import en from "javascript-time-ago/locale/en";
+
+TimeAgo.addDefaultLocale(en);
+
 let componentsContext = {};
 Components.filenames.forEach((fileName, i) => {
   let cleanName = fileName
