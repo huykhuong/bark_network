ac1eb03742f3cb11aab25957f258d0d3181ad227
huykhuong <scottkhuong@gmail.com>
2024-06-07 21:18:19 +0700

    Small stylings improvements

--- app/javascript/app/shared/PeopleColumn/FriendCard.tsx
+++ app/javascript/app/shared/PeopleColumn/FriendCard.tsx
@@ -55,15 +55,15 @@ const FriendCard: FC<Props> = ({
       key={id}
       className="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-transparent bg-clip-border border border-slate-200/80 p-2"
     >
-      <div className="relative flex items-center gap-4 pt-0 pb-8 mx-0 overflow-hidden bg-transparent shadow-none rounded-xl bg-clip-border">
+      <div className="relative w-full flex items-center gap-4 pt-0 pb-8 mx-0 overflow-hidden bg-transparent shadow-none rounded-xl bg-clip-border">
         <img
           src={avatar || avatarPlaceholder}
           alt={`user_${displayName}`}
-          className="relative inline-block h-[58px] w-[58px] !rounded-full  object-cover object-center"
+          className="relative inline-block h-[58px] w-[58px] !rounded-full object-cover object-center"
         />
         <div className="flex w-full flex-col gap-0.5">
           <div className="flex items-center justify-between">
-            <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-slate-800">
+            <h5 className="w-24 block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-slate-800 text-ellipsis overflow-hidden">
               {displayName}
             </h5>
           </div>
