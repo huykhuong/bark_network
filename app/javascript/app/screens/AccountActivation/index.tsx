7d4e7f9a80422fccaf0a24fa1ca241f8f5e3a777
huykhuong <scottkhuong@gmail.com>
2024-06-30 13:03:10 +0700

    Add logout button

--- app/javascript/app/screens/AccountActivation/index.tsx
+++ app/javascript/app/screens/AccountActivation/index.tsx
@@ -1,6 +1,7 @@
 import { UserContext } from "@contexts/User";
 import MainContainer from "@shared/MainContainer";
 import ScreenWrapper from "app/screens/ScreenWrapper";
+
 import { useContext } from "react";
 
 const AccountActivation = () => {
@@ -14,14 +15,30 @@ const AccountActivation = () => {
         <h1 className="text-3xl font-bold mb-5">Account Activation</h1>
         {confirmed && (
           <p>
-            Your account has been successfully activated. You can now log in
-            with your email and password.
+            Your account has been successfully activated. No further action is
+            needed.
           </p>
         )}
         {!confirmed && (
-          <a href="/confirmations" data-method="post" rel="nofollow">
-            Resend Activation Email
-          </a>
+          <>
+            <a
+              href="/confirmations"
+              data-method="post"
+              rel="nofollow"
+              className="text-blue-500 hover:text-blue-600"
+            >
+              Resend Activation Email
+            </a>
+
+            <a
+              href="/logout"
+              data-method="delete"
+              rel="nofollow"
+              className="block mt-5"
+            >
+              Logout
+            </a>
+          </>
         )}
       </MainContainer>
     </>
